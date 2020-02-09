c20cf86bdd7d0bafaf7e3d63e302501005a9a107
leonardodasilvamelo <leonardomelo.ti@gmail.com>
2020-02-08 23:03:00 -0300

    Primeiro commit

---
+++ Grpc/CalculadoraClient/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System;
+using Grpc.Core;
+using Calculadora;
+
+namespace CalculadoraClient
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+          
+
+            
+
+            var sair = false;
+            while (sair == false)
+            {
+
+                Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+                var client = new Operacao.OperacaoClient(channel);
+
+                Console.WriteLine("Digite o primeiro parâmetro!");
+                var parametro1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite a operação!(+ - / *)");
+                var operacao = Console.ReadLine();
+
+                Console.WriteLine("Digite o segundo parâmetro!");
+                var parametro2 = int.Parse(Console.ReadLine());
+
+                
+
+                if (operacao == "+")
+                {
+                    Console.WriteLine("Resultado: : " + Soma(client, parametro1, parametro2).ToString());
+                }
+                else
+                if (operacao == "-")
+                {
+                    Console.WriteLine("Resultado : " + Subtracao(client, parametro1, parametro2).ToString());
+                }
+                else
+                if (operacao == "/")
+                {
+                    Console.WriteLine("Resultado : " + Divisao(client, parametro1, parametro2).ToString());
+                }
+                else
+                if (operacao == "*")
+                {
+                    Console.WriteLine("Resultado : " + Multiplicao(client, parametro1, parametro2).ToString());
+                }
+                
+                channel.ShutdownAsync().Wait();
+                Console.WriteLine("Digite s para sair ou n para tentar novamente");
+
+               
+                sair = Console.ReadLine() == "s" ? true : false;
+
+
+                Console.WriteLine("++++++++++++++++++++++++++++++++++");
+                Console.WriteLine("");
+            }
+        }
+
+        public static int Soma( Operacao.OperacaoClient client, int parametro1, int parametro2)
+        {
+            return client.Somar(new ParametroRequest { Primeiro = parametro1, Segundo = parametro2 }).Resultado;
+        }
+
+        public static int Divisao(Operacao.OperacaoClient client, int parametro1, int parametro2)
+        {
+            return client.Dividir(new ParametroRequest { Primeiro = parametro1, Segundo = parametro2 }).Resultado;
+        }
+
+        public static int Multiplicao(Operacao.OperacaoClient client, int parametro1, int parametro2)
+        {
+            return client.Multiplicar(new ParametroRequest { Primeiro = parametro1, Segundo = parametro2 }).Resultado;
+        }
+
+        public static int Subtracao(Operacao.OperacaoClient client, int parametro1, int parametro2)
+        {
+            return client.Subtrair(new ParametroRequest { Primeiro = parametro1, Segundo = parametro2 }).Resultado;
+        }
+    }
+}
+
---
+++ Grpc/CalculadoraServer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Grpc.Core;
+using Calculadora;
+
+
+using System.Threading.Tasks;
+
+namespace CalculadoraServer
+{
+
+    class CalculadoraImpl : Operacao.OperacaoBase
+    {
+        
+        public override Task<ParametroReply> Somar(ParametroRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ParametroReply { Resultado = request.Primeiro + request.Segundo });
+        }
+
+        public override Task<ParametroReply> Dividir(ParametroRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ParametroReply { Resultado = request.Primeiro / request.Segundo });
+        }
+
+        public override Task<ParametroReply> Subtrair(ParametroRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ParametroReply { Resultado = request.Primeiro - request.Segundo });
+        }
+
+        public override Task<ParametroReply> Multiplicar(ParametroRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ParametroReply { Resultado = request.Primeiro * request.Segundo });
+        }
+
+    }
+
+    class Program
+    {
+        const int Port = 50051;
+
+        public static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { Operacao.BindService(new CalculadoraImpl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Greeter server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
